be6505246ec16a134a322f17791123b36ae05d39
Tiago Biazin <tiago.biazin02@gmail.com>
2024-03-28 18:44:53 -0300

    criando cabecalho

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
-  <script setup>
-  import { reactive } from 'vue';
-  import Cabecalho from '/Usuario/Desktop/testDataStone/desafioTest/src/components/Cabecalho.vue';
-  import CadastroClientes from '/Usuario/Desktop/testDataStone/desafioTest/src/components/CadastroCliente.vue';
-  import CadastroProduto from '/Usuario/Desktop/testDataStone/desafioTest/src/components/CadastroProduto.vue';
-  import Associa from '/Usuario/Desktop/testDataStone/desafioTest/src/components/Associa.vue';
-  import ListagemProduto from '/Usuario/Desktop/testDataStone/desafioTest/src/components/ListagemProdutos.vue';
-  import ListagemCliente from '/Usuario/Desktop/testDataStone/desafioTest/src/components/ListagemClientes.vue';
+<script setup>
+import { reactive } from 'vue';
+import Cabecalho from '/Usuario/Desktop/testDataStone/desafioTest/src/components/Cabecalho.vue';
+import CadastroClientes from '/Usuario/Desktop/testDataStone/desafioTest/src/components/CadastroCliente.vue';
+import CadastroProduto from '/Usuario/Desktop/testDataStone/desafioTest/src/components/CadastroProduto.vue';
+import Associa from '/Usuario/Desktop/testDataStone/desafioTest/src/components/Associa.vue';
+import ListagemProduto from '/Usuario/Desktop/testDataStone/desafioTest/src/components/ListagemProdutos.vue';
+import ListagemCliente from '/Usuario/Desktop/testDataStone/desafioTest/src/components/ListagemClientes.vue';
+
+const state = reactive({
+  isMenuOpen: false
+});
+
+const toggleMenu = () => {
+  state.isMenuOpen = !state.isMenuOpen;
+};
+
+const calcularProdutosSelecionados = (cliente) => {
+  if (cliente.produtos && cliente.produtos.length > 0) {
+    const nomesProdutos = cliente.produtos.map(produto => produto.nome);
+    return nomesProdutos.join(', ');
+  } else {
+    return 'Nenhum produto associado';
+  }
+};
 </script>
 
-  <template>
-    <h1>Ola</h1>
-  </template>
+<template>
+  <div>
+    <Cabecalho :toggle-menu="toggleMenu" />
+    <CadastroClientes />
+    <CadastroProduto />
+    <Associa />
+    <ListagemProduto />
+    <ListagemCliente />
+  </div>
+</template>
 
-  <style scoped>
+<style scoped>
 
-  </style>
+</style>
